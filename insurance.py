730d176d13bd9fb5c96866cbe9295623605822b2
b-giblin <91639287+b-giblin@users.noreply.github.com>
2023-08-15 16:15:45 -0400

    Initial Commit

--- insurance.py
+++ insurance.py
@@ -6,7 +6,7 @@ company_name = input("What is your insurance companies name?")
 emplyee_amount = input("How many employees do you have?")
 location = input("What is your city, counrty, or state?")
 lowest_price = input("What is your lowest priced policy?")
-highest_price = input("What is your highest priced policy? ")
+highest_price = input("What is your highest priced policy?")
 
 print(f"We are {company_name} located in {location}. Our {emplyee_amount} employees can help you find the policy that is right for you with policies ranging from {lowest_price} to {highest_price} per month!")
 
